4c54ccf670926e63917290d62e7aab49e998cba1
ckchaudhary <ckchaudhary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-11 09:53:33 +0000

    my first commit of the plugin
    git-svn-id: https://plugins.svn.wordpress.org/fb-like-notification-for-buddypress/trunk@695838 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bp-fb-like-notification.php
@@ -0,0 +1,70 @@
+<?php 
+/* 
+Plugin Name: FB like notification for buddypress
+Plugin URI: http://emediaidentity.com/bp-fb-like-notification/
+Description: This Buddypress plugin updates notification and browser window title while user is away
+Version: 0.1
+Revision Date: 08 02, 2013
+Requires at least: WP 3.4.1, BuddyPress 1.6
+Tested up to: WP 3.2.1, BuddyPress 1.6
+License: GNU General Public License 2.0 (GPL) http://www.gnu.org/licenses/gpl.html
+Author: ckchaudhary
+Author URI: http://webdeveloperswall.com/
+Network: true
+*/
+
+//do anything only if buddypress is active
+add_action( 'bp_include', function() { new BP_FB_like_notification(); } );
+
+class BP_FB_like_notification {
+	function __construct() {
+		add_action( 'wp_enqueue_scripts',  array( $this, 'enqueue_scripts' ) );
+		add_action( 'wp_ajax_bfln_get_notification',  array( $this, 'ajax_get_notifications' ) );
+	}
+	
+	function enqueue_scripts(){
+		wp_enqueue_script(
+			"bfln_main",
+			path_join( WP_PLUGIN_URL, basename( dirname( __FILE__ ) )."/bp-fb-like-notification_v4.js" ),
+			array( 'jquery' )
+		);
+		
+		/*the time interval : after each interval an ajax request to check for new notification is triggered
+		default is 2 minutes : 2*60*1000: computers understand milliseconds only :) 
+		if you want change 2 minutes to something else, replace 2 with something else in the line below*/
+		$time = 2*60*1000;
+
+		$arguments = array(
+			"action"	=> "bfln_get_notification",
+			"time"		=> $time,
+			"doctitle"	=> "", /*so that javascript can access it without running into null pointer exceptions*/
+			"newdoctitle"	=> "", /*so that javascript can access it without running into null pointer exceptions*/
+			"newnotification"	=> 0 /*so that javascript can access it without running into null pointer exceptions*/
+		);
+
+		wp_localize_script( "bfln_main", "BFLN_", $arguments );
+	}
+
+	
+	function ajax_get_notifications(){
+		$retVal = array(
+			"count"	=> 0,
+			"notifications"	=> ""
+		);
+		
+		if ( !is_user_logged_in() ){
+			echo json_encode($retVal);
+			exit();
+		}
+			
+		$user_id = bp_loggedin_user_id();
+		$notifictions = bp_core_get_notifications_for_user( $user_id , 'object');
+		if( $notifictions && !empty( $notifictions ) ){
+			$retVal["count"] = count($notifictions);
+			$retVal["notifications"] = $notifictions;
+		}
+		echo json_encode($retVal);
+		exit();
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+﻿=== FB like notification for buddypress ===
+Contributors: ckchaudhary 
+Donate link:http://emediaidentity.com
+Tags: buddypress, notification, auto refresh notification
+Requires at least: 1.6
+Tested up to: 1.6.5
+Stable tag: 0.1
+
+This Buddypress plugin updates notification and browser window title while user is away
+
+== Description ==
+
+The plugin repeatedly checks for new notifications for the loggedin user and if any new notification is found, it does 3 things:
+
+1) **Updates the browser window title**( e.g: \'members|mydomain.com\' becomes \'(2) members|mydomain.com\' ) and blinks/switches repeatedly between the old and new title
+
+2) **Updates the notification menu:**
+
+ - adds new notification(if any) items to the list
+
+ - updates the existing ones(if any). E.g: from \'You have 1 new message\' to \'You have 2 new messages\'
+
+ 
+
+**Note: **The second (updating notification menu) has been developed according to the markup of default buddybar/wp-admin bar, so if your theme doesn\'t have one, or have a custom notification list, it wont work, althoug the broswer title will update just fine.
+
+The plugin is still in testing mode, and i wouldn\'t suggest you use it on an \'actual\' website straight away.
+
+Please report bugs and suggestions.
+
+ 
+
+== Installation ==
+
+Extract the zip file and just drop the contents in the wp-content/plugins/ directory of your WordPress installation and then activate the Plugin from Plugins page.
